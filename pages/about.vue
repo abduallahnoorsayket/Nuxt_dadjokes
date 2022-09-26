33e4659831360067c38eee15eafe5ec3183e046b
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2022-09-26 16:56:31 +0600

    index page modified

---
+++ pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>This the about page of dadjokes</h1>
+    <p>
+      It is a long established fact that a reader will be distracted by the
+      readable content of a page when looking at its layout. The point of using
+      Lorem Ipsum is that it has a more-or-less normal distribution of letters
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About page",
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: "description",
+          name: "description",
+          content: "My ABout description of dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>This is the main index page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  head() {
+    return {
+      title: "Main index page",
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: "description",
+          name: "description",
+          content: "My ABout description of dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
