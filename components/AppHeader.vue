5aafe417d99b4377b609b9627c19c561d8f4a17a
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2022-09-27 17:44:09 +0600

    appheader added

---
+++ components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <nuxt-link to="/home" class="nav-item nav-link active">
+            hello</nuxt-link
+          >
+          <a class="nav-item nav-link" href="#">Features</a>
+          <a class="nav-item nav-link" href="#">Pricing</a>
+          <a class="nav-item nav-link disabled" href="#">Disabled</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style></style>
