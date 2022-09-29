b104fcce476f161fa9f00b1abae2479c0613c707
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2022-09-29 17:22:53 +0600

    axios added

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand">Dad Jokes</a>
 
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <nuxt-link to="/home" class="nav-item nav-link active">
-            hello</nuxt-link
+          <nuxt-link to="/" class="nav-item nav-link active"> Home</nuxt-link>
+          <nuxt-link to="/Jokes" class="nav-item nav-link active">
+            Jokes</nuxt-link
           >
-          <a class="nav-item nav-link" href="#">Features</a>
-          <a class="nav-item nav-link" href="#">Pricing</a>
-          <a class="nav-item nav-link disabled" href="#">Disabled</a>
+          <nuxt-link to="/about" class="nav-item nav-link active">
+            About</nuxt-link
+          >
         </div>
       </div>
     </nav>
--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -5,7 +5,42 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      jokes: [],
+    };
+  },
+  created() {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    try {
+      const res = axios.get("https://icanhazdadjoke.com/search", config);
+      console.log("24", res.data.results);
+      this.jokes = res.data.results;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  head() {
+    return {
+      title: "Dad Jokes ",
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: "description",
+          name: "description",
+          content: "My ABout description of dad jokes",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style></style>
